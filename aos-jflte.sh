07e7261b4c6317a332db712271b9aa9881fa1a4d
treken <chupekevin@gmail.com>
2014-06-12 17:33:28 -0500

    Update aos-jflte.sh

--- aos-jflte.sh
+++ aos-jflte.sh
@@ -1,1 +1,12 @@
-export BSPEED=21 && brunch aos_jflte-userdebug
+#!/bin/bash
+## Build Automation Scripts
+##
+## Copywrite 2014 - Donald Hoskins <grommish@gmail.com>
+## on behalf of Team Octos et al.
+
+## Check to see if there are build args
+## First Argument is for the -j concurrent build threads - Defaults to -j21 unless
+## you set it otherwise.  Careful, or it will melt your machine!
+. build/envsetup &&
+export BSPEED=21 && 
+brunch aos_jflte-userdebug
